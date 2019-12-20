0a2070200c646e8a9a2d92e662ac31bcd1c6d2b5
sol <Sergio.ORTEGA-LOPEZ@ext-ec-europa.eu>
2019-12-20 15:42:22 +0100

    Section 2: The basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  /*
+  styles: [`
+    h3 {
+      color:dodgerblue;
+    }
+    `]
+  */
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'completeguide';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+// import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccesAlertComponent } from './succes-alert/succes-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccesAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
